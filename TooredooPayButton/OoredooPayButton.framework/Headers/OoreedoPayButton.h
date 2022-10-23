3a2d2fd920b3b048930fc494293ea676a4c39c5b
Hazem Mohamed <hazem.mohamed@paysky.io>
2022-10-23 12:10:48 +0200

    initial commit

---
+++ TooredooPayButton/OoredooPayButton.framework/Headers/OoreedoPayButton.h
@@ -0,0 +1,18 @@
+//
+//  OoreedoPayButton.h
+//  OoreedoPayButton
+//
+//  Created by Nada Kamel on 02/09/2022.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for OoreedoPayButton.
+FOUNDATION_EXPORT double OoreedoPayButtonVersionNumber;
+
+//! Project version string for OoreedoPayButton.
+FOUNDATION_EXPORT const unsigned char OoreedoPayButtonVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <OoreedoPayButton/PublicHeader.h>
+
+
---
+++ TooredooPayButton/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TooredooPayButton
+//
+//  Created by Hazem-Mohamed on 23/10/2022.
+//
+
+import UIKit
+import OoredooPayButton
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
